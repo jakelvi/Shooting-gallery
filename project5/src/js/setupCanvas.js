058025f23bfa130d5e92b20ca2d0c15177f854ed
jakelvi <132336850+jakelvi@users.noreply.github.com>
2024-02-21 20:37:53 +0200

    Delete project5/src directory

--- project5/src/js/setupCanvas.js
+++
@@ -1,9 +0,0 @@
-const setupCanvas = (canvasId) => {
-  const canvas = document.getElementById(canvasId);
-  const ctx = canvas.getContext("2d");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  return { canvas, ctx };
-};
-
-export default setupCanvas;
